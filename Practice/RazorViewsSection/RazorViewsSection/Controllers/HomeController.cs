1161724033f012fb2faf485610ff5623fee9c801
3mrotaha <amr.taha1261@gmail.com>
2026-02-15 15:42:01 +0200

    Razor views section uploaded

---
+++ Practice/RazorViewsSection/RazorViewsSection/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorViewsSection.Models;
+
+namespace RazorViewsSection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Person> poeple = new List<Person>()
+        {
+            new Person() {Name = "Amr", Email = "asdf@gmail.com", Dob = DateTime.Now.AddYears(-25), Gender = Gender.Male},
+            new Person() {Name = "Linda", Email = "dsa@gmail.com", Dob = DateTime.Now.AddYears(-55), Gender = Gender.Female},
+            new Person() {Name = "Mahmoud", Email = "dd@gmail.com", Dob = DateTime.Now.AddYears(-13), Gender = Gender.Male},
+            new Person() {Name = "Sara", Email = "aaasdf@gmail.com", Dob = DateTime.Now.AddYears(-20), Gender = Gender.Female}
+
+        };
+
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.appTitle = "ASP.NET Core Application / Views section";
+            /**
+             * the view is stored in Views/Home/Index.cshtml by default
+             * maps to Views/{Controller Name}/{Action}.cshtml
+             * 
+             * can be renamed View("abc") -> which maps to Views/Home/abc.cshtml
+             */
+            return View("Index", poeple);
+        }
+
+        [Route("/person-details/{name:maxlength(100):minlength(3)}")]
+        public IActionResult Details([FromRoute] string name)
+        {
+            var person = poeple.Where(p => p.Name == name).FirstOrDefault();
+
+            return View("Details", person);
+        }
+    }
+}
---
+++ Practice/RazorViewsSection/RazorViewsSection/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace RazorViewsSection.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime? Dob { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+
+    public enum Gender
+    {
+        Male, Female
+    }
+}
