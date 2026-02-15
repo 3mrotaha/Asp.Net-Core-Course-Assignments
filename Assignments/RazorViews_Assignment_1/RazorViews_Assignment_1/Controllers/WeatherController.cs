20fe26e39b199ac95ffc77f1e35a53845309340d
3mrotaha <amr.taha1261@gmail.com>
2026-02-15 15:41:19 +0200

    Razor views assignment uploaded

---
+++ Assignments/RazorViews_Assignment_1/RazorViews_Assignment_1/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorViews_Assignment_1.Models;
+
+namespace RazorViews_Assignment_1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> cities = new List<CityWeather>()
+        {
+            new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+            new CityWeather() {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82},
+            new CityWeather() {CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+        };
+
+        [Route("/")]
+        public IActionResult Home()
+        {
+            ViewBag.pageTitle = "Home";
+            return View("HomeView", cities);
+        }
+
+        [Route("/weather/{cityCode:length(3, 3)}")]
+        public IActionResult CityWeather([FromRoute] string cityCode)
+        {
+            CityWeather? cityWeather = cities.Where(c => c.CityUniqueCode == cityCode).FirstOrDefault();
+            ViewBag.pageTitle = $"{cityWeather?.CityName} Weather";
+            return View("CityWeatherView", cityWeather);
+        }
+    }
+}
---
+++ Assignments/RazorViews_Assignment_1/RazorViews_Assignment_1/Models/CityWeather.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorViews_Assignment_1.Models
+{
+    public class CityWeather
+    {        
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
